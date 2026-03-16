fd2a3f2f453f0cf5fbf0e9e4a582ce46420b9610
eduardo madrigal <eduardo07a@hotmail.com>
2026-03-16 12:47:16 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinalL/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProyectoFinalL.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Login(string correo, string password)
+        {
+            string conexion = ConfigurationManager.ConnectionStrings["MathRiddlesDBEntities"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                string query = "SELECT COUNT(*) FROM Usuarios WHERE Correo=@correo AND PasswordHash=@password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@correo", correo);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                con.Open();
+                int existe = (int)cmd.ExecuteScalar();
+
+                if (existe > 0)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Error = "Usuario incorrecto";
+                    return View(); 
+                }
+            }
+        }
+    }
+}
