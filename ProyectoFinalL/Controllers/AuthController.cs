9e2ca8db72fc7aae1f8de9038ba8f28c3172c713
eduardo07a <Eduardo07a@hotmail.com>
2026-03-21 18:17:42 -0600

    Se arreglo unos problemas con el login y el register

--- ProyectoFinalL/Controllers/AuthController.cs
+++ ProyectoFinalL/Controllers/AuthController.cs
@@ -18,43 +18,52 @@ namespace ProyectoFinalL.Controllers
         [HttpPost]
         public ActionResult Login(string correo, string password)
         {
+           
             var user = db.Usuarios
                 .FirstOrDefault(x => x.Correo == correo && x.Password == password);
 
             if (user != null)
             {
-                Session["Usuario"] = user.Nombre; 
+                Session["Usuario"] = user.Nombre;
                 Session["UsuarioId"] = user.IdUsuario;
                 Session["EsAdmin"] = user.EsAdmin;
 
                 return RedirectToAction("Index", "Home");
             }
 
-            ViewBag.Error = "Usuario o contraseña incorrectos";
-            return View();
-        }
-
-        public ActionResult Register()
-        {
+       
+            ViewBag.Error = "USUARIO O CONTRASEÑA INCORRECTOS";
+            ViewBag.Modo = "login";
             return View();
         }
 
         [HttpPost]
         public ActionResult Register(string correo, string password, string nombre)
         {
+            
+            var existe = db.Usuarios.Any(u => u.Correo == correo);
+
+            if (existe)
+            {
+                ViewBag.Error = "ESTE CORREO YA ESTÁ REGISTRADO";
+                ViewBag.Modo = "registro";
+                return View("Login"); 
+            }
+
             var nuevo = new Usuario
             {
                 Correo = correo,
                 Password = password,
-                Nombre = nombre, 
-                EsAdmin = false 
+                Nombre = nombre,
+                EsAdmin = false
             };
 
             bool exito = _repo.RegistrarUsuario(nuevo);
             if (exito) return RedirectToAction("Login");
 
-            ViewBag.Error = "Error al registrar. Intenta con otro correo.";
-            return View();
+            ViewBag.Error = "ERROR AL REGISTRAR. INTENTA DE NUEVO.";
+            ViewBag.Modo = "registro";
+            return View("Login");
         }
 
         public ActionResult Logout()
